a3c46f6e5b288735c6c8bd3a9f63605e434cc4e5
김동현 <ehdgus458@Gmail.com>
2025-08-06 10:13:30 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.13 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,13 +2,7 @@ class Solution {
     public String solution(int n) {
         String answer = "";
         
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 1) {
-                answer += "수";
-            } else {
-                answer += "박";
-            }
-        }
+        answer = "수박".repeat((n + 1) / 2).substring(0, n);
         
         return answer;
     }
