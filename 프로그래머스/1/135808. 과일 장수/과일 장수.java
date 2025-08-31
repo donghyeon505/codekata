4acbfa062a5fdce979eb5ea042f03cc14e8fd456
김동현 <ehdgus458@Gmail.com>
2025-08-31 11:14:40 +0900

    [level 1] Title: 과일 장수, Time: 44.53 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -5,9 +5,8 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(score);
-        int length = score.length;
         
-        for (int i = length; i >= m; i -= m) {
+        for (int i = score.length; i >= m; i -= m) {
             answer += score[i - m] * m;
         }
         
